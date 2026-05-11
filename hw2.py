c602937d8e23cc5815eb20b25ec7494812827866
SICH0_ATEL1ER <satcoff28@gmail.com>
2026-05-11 20:24:29 +0900

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,10 +1,16 @@
-from gpiozero import Button, LED
+from gpiozero import LED, Button
 from signal import pause
 
 led = LED(14)
 button = Button(4)
 
+print("Program started. LED (GPIO 14) is controlled by input on GPIO 4.")
+print("Current input value (0 or 1):", button.value)
+
 button.when_pressed = led.on
 button.when_released = led.off
 
-pause()
+try:
+    pause()
+except KeyboardInterrupt:
+    print("\nProgram terminated.")
